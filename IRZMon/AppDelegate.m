11e1e9513f96860a16219593f97b9090c81d38c0
yarry <yarryp@gmail.com>
2012-06-22 01:51:07 +0400

    Initial Commit

---
+++ IRZMon/AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.m
+//  IRZMon
+//
+//  Created by  on 22.06.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
